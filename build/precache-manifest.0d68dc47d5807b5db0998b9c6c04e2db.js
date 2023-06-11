b5728a9afad25cc011c98c529ed5ac5fd2a4bda6
tihostyn <Tijs.Hostyn@UGent.be>
2023-06-12 00:26:24 +0200

    build

---
+++ build/precache-manifest.0d68dc47d5807b5db0998b9c6c04e2db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "942e63ebf3fbc610daca",
+    "url": "/profile/static/css/main.8970645d.chunk.css"
+  },
+  {
+    "revision": "942e63ebf3fbc610daca",
+    "url": "/profile/static/js/main.942e63eb.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/profile/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/profile/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "f99777ca1da1c313162c",
+    "url": "/profile/static/js/runtime~main.f99777ca.js"
+  },
+  {
+    "revision": "197ecb468dc51b8930e1d4e003d049d8",
+    "url": "/profile/index.html"
+  }
+];
